12f747d02ae2dc5bce7e9263ac7be49eaaf117b7
JY K <kjyong1983@gmail.com>
2018-12-19 16:13:25 +0900

    듀얼 모니터 테스트를 위한 준비

--- Assets/Scripts/RenderSelector.cs
+++ Assets/Scripts/RenderSelector.cs
@@ -2,18 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RenderTest : MonoBehaviour {
+public class RenderSelector : MonoBehaviour {
 
     public Camera mainDisplayCam;
     public LayerMask doNotRender;
 
-	// Use this for initialization
 	void Start () {
         mainDisplayCam.cullingMask = ~doNotRender;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
---
+++ Assets/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public GameObject testPrefab;
+    
+    public void OnCreatButtonClick()
+    {
+        Instantiate(testPrefab, transform.position, Quaternion.identity);
+    }
+
+}
