da258e3aa523cd97b70c21fe7f449047d239e9a5
JY K <kjyong1983@gmail.com>
2018-12-19 17:21:08 +0900

    UI 리소스 업로드, UI 적용 및 확대 축소 기능 추가.

---
+++ Assets/UIZoom.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIZoom : MonoBehaviour, IScrollHandler {
+
+    public RectTransform rectTransform;
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        if (eventData.scrollDelta != Vector2.zero)
+        {
+            Debug.Log(eventData.scrollDelta);
+            if (eventData.scrollDelta.y > 0)
+            {
+                rectTransform.localScale *= 1.1f;
+            }
+            else if (eventData.scrollDelta.y < 0)
+            {
+                rectTransform.localScale *= 0.9f;
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
