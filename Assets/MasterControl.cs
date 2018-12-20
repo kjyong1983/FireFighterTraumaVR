de33e4c154cf6234fcf4f8d31af6e589ef63ef62
jyk <kjyong1983@gmail.com>
2018-12-20 21:23:43 +0900

    마스터 컨트롤 추가 및 쓰레기통과 폭발 이벤트 연결.

---
+++ Assets/MasterControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterControl : MonoBehaviour
+{
+    public GameObject[] controlObjects;
+
+    public void OnControlObjectButtonClick(int num)
+    {
+        Debug.Log("OnControlObjectButtonClick " + num);
+        controlObjects[num].SendMessage("ActivateObject");
+    }
+}
