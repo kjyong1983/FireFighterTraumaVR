6b771405a19386152ad24183efda58cbf6e04fca
jyk <kjyong1983@gmail.com>
2018-12-21 00:53:49 +0900

    스크립트 정리, 두번쨰 문 애니메이션 추가, 꺼지는 불과 물 호스 추가

---
+++ Assets/03. Scripts/ControlDrop.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDrop : MonoBehaviour
+{
+    public AudioSource audio;
+
+    public void ActivateObject()
+    {
+        GetComponent<DropEvent>().Drop();
+        audio.Play();
+    }
+}
--- Assets/03. Scripts/Corridor/Waypoint.cs
+++ Assets/03. Scripts/Corridor/Waypoint.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Transform[] points = GetComponentsInChildren<Transform>();
-        int next = 1;
-        Vector3 cPos = points[next].position;
-        Vector3 nPos;
-        //다음 포인트 위치까지 드로우라인
-        for (int i = 2; i < points.Length; i++)
-        {
-            nPos = points[i].position;
-            Gizmos.DrawLine(cPos, nPos);
-            cPos = nPos;
-        }
-    }
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.green;
+    //    Transform[] points = GetComponentsInChildren<Transform>();
+    //    int next = 1;
+    //    Vector3 cPos = points[next].position;
+    //    Vector3 nPos;
+    //    //다음 포인트 위치까지 드로우라인
+    //    for (int i = 2; i < points.Length; i++)
+    //    {
+    //        nPos = points[i].position;
+    //        Gizmos.DrawLine(cPos, nPos);
+    //        cPos = nPos;
+    //    }
+    //}
 }
