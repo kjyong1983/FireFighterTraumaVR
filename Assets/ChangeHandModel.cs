dde3ae246e92fbae7bcc503e70389a91a11043d7
JY K <kjyong1983@gmail.com>
2018-12-20 15:37:28 +0900

    Amplify Shader Editor 에셋 추가

---
+++ Assets/ChangeHandModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeHandModel : MonoBehaviour {
+
+    public GameObject rightHand;
+    public GameObject leftHand;
+
+    public GameObject[] hands;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+
+        if (other.gameObject == rightHand)
+        {
+            rightHand = hands[1];
+        }
+
+        if (other.gameObject == leftHand)
+        {
+            leftHand = hands[1];
+        }
+    }
+}
