2890793f2f13c1ae27b2ef964c9b3a56bf2a9913
jyk <kjyong1983@gmail.com>
2018-12-21 03:42:10 +0900

    5번 사람 도망 이벤트 추가. 물 호스 교체 및 쏘기 추가.

---
+++ Assets/ControlPeople.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPeople : MonoBehaviour
+{
+    public Move[] people;
+
+    public void ActivateObject()
+    {
+        Debug.Log("ActivateObject");
+        for (int i = 0; i < people.Length; i++)
+        {
+            people[i].canExit = true;
+        }
+    }
+}
