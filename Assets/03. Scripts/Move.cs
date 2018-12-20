7a2ec26770cba41ba944fca59cfc57744250a332
JY K <kjyong1983@gmail.com>
2018-12-20 16:20:26 +0900

    복도 리소스 추가.

---
+++ Assets/03. Scripts/Move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public float damping = 3.0f;
+    private Transform tr;
+    private Transform[] points;
+    private int nextIdx = 1;
+    enum STATE {  MOVE, STOP}
+    private STATE state;
+
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+        points = GameObject.Find("Waypoints").GetComponentsInChildren<Transform>();
+        state = STATE.MOVE;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = points[nextIdx].position - tr.position;
+        Quaternion rot = Quaternion.LookRotation(direction);
+        tr.rotation = Quaternion.Slerp(tr.rotation, rot, Time.deltaTime * damping);
+        tr.Translate(Vector3.forward * Time.deltaTime * speed);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("WAY_POINT"))
+        {
+            if (nextIdx + 1 >= points.Length)
+            {
+                //state = STATE.STOP;
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                ++nextIdx;
+            }
+        }
+    }
+}
