38db407f856335df61a7ae5347fd996ee46231fc
JY K <kjyong1983@gmail.com>
2018-12-20 09:49:21 +0900

    페이드인/아웃 테스트중

---
+++ Assets/Scripts/FadeInOut.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInOut : MonoBehaviour {
+
+    public Image image;
+    public Color black;
+    public Color transparent;
+    public float _duration;
+
+    public void OnFadeInButtonClick()
+    {
+        StartCoroutine(FadeIn(_duration));
+    }
+
+    public void OnFadeOutButtonClick()
+    {
+        StartCoroutine(FadeOut(_duration));
+    }
+
+    IEnumerator FadeOut(float duration)
+    {
+        float timer = 0;
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            image.color = new Color(0,0,0, (timer / duration));
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeIn(float duration)
+    {
+        float timer = 0;
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            image.color = new Color(0, 0, 0, 1f - (timer / duration));
+            yield return null;
+        }
+    }
+
+}
