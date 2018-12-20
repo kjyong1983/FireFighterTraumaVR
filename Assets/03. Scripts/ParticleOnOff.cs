61d53eda0524f272dd3d768b4aaed20d7f57dfc8
JY K <kjyong1983@gmail.com>
2018-12-20 18:51:59 +0900

    물 호스 및 불 프리팹 추가.

---
+++ Assets/03. Scripts/ParticleOnOff.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleOnOff : MonoBehaviour {
+    public ParticleSystem pat;
+    private bool isOn= false;
+	// Use this for initialization
+	void Start () {
+        pat.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            if (!isOn)
+            {
+                pat.Play();
+                isOn = true;
+            }
+            else
+            {
+                pat.Stop();
+                isOn = false;
+            }
+        }
+    }
+}
