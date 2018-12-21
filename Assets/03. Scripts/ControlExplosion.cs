bb05f162e3a6ee70099a44291374e4aed2d69b4c
jyk <kjyong1983@gmail.com>
2018-12-21 10:02:01 +0900

    카메라 흔들기 추가.

--- Assets/03. Scripts/ControlExplosion.cs
+++ Assets/03. Scripts/ControlExplosion.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class ControlExplosion : MonoBehaviour
 {
+    public  CameraShake shake;
+
     public void ActivateObject()
     {
         GetComponent<ExplosiveTest>().Explosion();
+        shake.ShakeCamera(1f, 1f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            shake.ShakeCamera(1f, 1f);
+        }
     }
 }
