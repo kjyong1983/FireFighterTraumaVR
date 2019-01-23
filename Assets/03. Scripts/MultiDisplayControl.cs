d25cfebe791913ed42084a9073997a2c960154b3
Yuriver - in Gwanggyo(14) <rkfkadlek03@naver.com>
2019-01-23 17:42:07 +0900

    멀티 디스플레이 지원하도록 스크립트 추가. 재시작 기능 추가.

---
+++ Assets/03. Scripts/MultiDisplayControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiDisplayControl : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("displays connected: " + Display.displays.Length);
+        // Display.displays[0] is the primary, default display and is always ON.
+
+        for (int i = 0; i < Display.displays.Length; i++)
+        {
+            if (Display.displays[i] != null)
+            {
+                Debug.Log(Display.displays[i].ToString());
+                Display.displays[i].Activate();
+            }
+        }
+
+        // Check if additional displays are available and activate each.
+        //if (Display.displays.Length > 1)
+        //    Display.displays[1].Activate();
+        //if (Display.displays.Length > 2)
+        //    Display.displays[2].Activate();
+    }
+}
